ba9add8e5a00a2e64707efa4c5807caba0bc3f0c
aatmannbc <aatmannred@gmail.com>
2024-11-12 10:25:09 -0500

    Skills updated

--- src/Skills.js
+++ src/Skills.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import './Skills.css'; // Make sure to create and import the CSS file
 
 function Skills() {
+  const skills = [
+    'Python (Most Proficient)',
+    'Kotlin',
+    'Java',
+    'C',
+    'JavaScript',
+    '.NET',
+    'SQL',
+    'R',
+    'HTML & CSS',
+    'MATLAB',
+    'Pandas',
+    'NumPy',
+    'Git'
+  ];
+
   return (
     <section id="skills">
       <h2>Skills</h2>
       <ul>
-        <li>Skill 1</li>
-        <li>Skill 2</li>
-        <li>Skill 3</li>
+        {skills.map((skill, index) => (
+          <li key={index}>
+            <span>{skill}</span>
+            <input type="range" min="1" max="5" defaultValue="3" className="slider" disabled />
+          </li>
+        ))}
       </ul>
     </section>
   );
